cdec4798f7512b60ca12880a33f9595a655268a7
Stephen Godfrey <sgodfrey@omadi.com>
2022-09-05 18:39:03 -0600

    typo

--- 6_terraform/dev/roles.tf
+++ 6_terraform/dev/roles.tf
@@ -1,7 +1,6 @@
 module "roles" {
-  source            = "../modules/eks"
+  source            = "../modules/dev"
 
   #setting this as a variable so we don't repeat ourselves
   group_path = "/ancient-beasts/"
-  
 }
